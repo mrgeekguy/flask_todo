f54c9ffc8d67764f76f2ab2a44f330e59f7a7587
mrgeekguy <mrgeekguymd@gmail.com>
2019-10-29 16:43:09 -0400

    Functioning

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def hello_world():
 
 @app.route("/todo")
 def todo():
-    return render_template("todo.html")
+    with open("./data/data.txt", "r+") as f:
+        todo_list = f.read().split("\n")
+    return render_template("./templates/todo.html", my_data=todo_list)
 
 @app.route("/submit")
 def submit():
@@ -16,12 +18,12 @@ def submit():
 
 @app.route("/submit_form", methods=["POST"])
 def submit_form():
-    data = (f'Topic: { request.form["topic"] } Info: {request.form["info"]}\n')
-    
+    data = (f'Topic: {request.form["topic"]} Info: {request.form["info"]}\n')
+    submit_data(data)
     return redirect("/todo")
 
 def submit_data(data):
-    with open("./data/data.txt", "w+") as f:
+    with open("./data/data.txt", "a") as f:
         f.write(data)
 
 if __name__ == "__main__":
